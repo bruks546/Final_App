8056b38e4711f5aed751ddc55e487195a73feeff
edwingariza <bruks546@gmail.com>
2017-04-11 17:09:37 -0500

    Created database

--- app/src/main/java/com/example/edwingariza/franssenpropertiesinc/DbHandler.java
+++ app/src/main/java/com/example/edwingariza/franssenpropertiesinc/DbHandler.java
@@ -21,6 +21,81 @@ public class DbHandler extends SQLiteOpenHelper{
     private static final String STATE_AP = "state";
     private static final String ZIP_AP = "zip";
     private static final String PHONE_AP = "phone";
+//First Relative
+    private static final String NAME_RELATIVE_1 = "rel_name_1";
+    private static final String RELATIONSHIP_1 = "relationship_1";
+    private static final String ADDRESS_REL_1 = "address_rel_1";
+    private static final String RELATIVE_PHONE_1 = "relative_phone_1";
+    //Second relative
+    private static final String NAME_RELATIVE_2 = "rel_name_2";
+    private static final String RELATIONSHIP_2 = "relationship_2";
+    private static final String ADDRESS_REL_2 = "address_rel_2";
+    private static final String RELATIVE_PHONE_2 = "relative_phone_2";
+    //Hud previws name
+    private static final String PREVIEWS_PROPERTY_NAME = "previews_name";
+    private static final String PREVIEWS_PROPERTY_NUMBER = "previews_number";
+
+    //Continue B
+    private static final String UNDER_NAME = "under_name";
+    private static final String WHEN = "when_have";
+    private static final String CURRENT_LANDLORD = "landlord_name";
+    private static final String LANDLORD_PHONE = "landlord_phone";
+    private static final String PRE_LANDLORD_NAME = "pre_landlord_name";
+    private static final String PRE_LANDLORD_PHONE = "pre_landlord_phone";
+
+    //Continue C
+    private static final String NAME_HOUSEHOLD_1 = "household_name_1";
+    private static final String HOUSEHOLD_RELATIONSHIP_1 = "household_relationship_1";
+    private static final String HOUSEHOLD_BIRTH_1 = "household_birth_name_1";
+    private static final String HOUSEHOLD_AGE_1 = "household_age_1";
+    private static final String HOUSEHOLD_SSN_1 = "household_ssn_1";
+
+    private static final String NAME_HOUSEHOLD_2 = "household_name_2";
+    private static final String HOUSEHOLD_RELATIONSHIP_2 = "household_relationship_";
+    private static final String HOUSEHOLD_BIRTH_2 = "household_birth_name_2";
+    private static final String HOUSEHOLD_AGE_2 = "household_age_2";
+    private static final String HOUSEHOLD_SSN_2 = "household_ssn_2";
+
+    private static final String NAME_HOUSEHOLD_3 = "household_name_3";
+    private static final String HOUSEHOLD_RELATIONSHIP_3 = "household_relationship_3";
+    private static final String HOUSEHOLD_BIRTH_3 = "household_birth_name_3";
+    private static final String HOUSEHOLD_AGE_3 = "household_age_3";
+    private static final String HOUSEHOLD_SSN_3 = "household_ssn_3";
+
+    private static final String NAME_HOUSEHOLD_4 = "household_name_4";
+    private static final String HOUSEHOLD_RELATIONSHIP_4 = "household_relationship_4";
+    private static final String HOUSEHOLD_BIRTH_4 = "household_birth_name_4";
+    private static final String HOUSEHOLD_AGE_4 = "household_age_4";
+    private static final String HOUSEHOLD_SSN_4 = "household_ssn_4";
+
+    private static final String HOUSEHOLD_HANDY = "household_handy";
+
+    //Continue D
+    private static final String INCOME_NAME_1 = "name_income_1";
+    private static final String INCOME_SOURCE_1 = "income_source_1";
+    private static final String INCOME_AMOUNT_1 = "income_amount_1";
+
+    private static final String INCOME_NAME_2 = "name_income_2";
+    private static final String INCOME_SOURCE_2 = "income_source_2";
+    private static final String INCOME_AMOUNT_2 = "income_amount_2";
+
+    private static final String INCOME_NAME_3 = "name_income_3";
+    private static final String INCOME_SOURCE_3 = "income_source_3";
+    private static final String INCOME_AMOUNT_3 = "income_amount_3";
+
+    //Continue E
+
+    private static final String BANK_NAME_CHECKING = "bank_name_checking";
+    private static final String BANK_NAME_SAVING = "bank_name_saving";
+    private static final String PROPERTY_VALUATION = "property_val";
+    private static final String MARKET_VALUE_DISPOSED = "market_val_dis";
+
+    private static final String VEHICLE_MAKE = "vehicle_made";
+    private static final String VEHICLE_MODEL = "vehicle_model";
+    private static final String VEHICLE_LICENSE = "vehicle_license";
+    private static final String VEHICLE_YEAR = "vehicle_year";
+
+    private static final String HEAR_PROPERTY = "hear_property";
 
 
 
